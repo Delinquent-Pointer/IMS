16d7a7f2bc4141a2bef3a433c9181c8a14610644
Delinquent-Pointer <aperturescience68k@live.com>
2025-02-25 23:17:37 -0800

    IT Dashboard Added
    added MVP IT manager dashboard
    - needs more features later
    - additional UserAccount table column

--- obj/Debug/net9.0/IMS.AssemblyInfo.cs
+++ obj/Debug/net9.0/IMS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed2001d33d6e3051789e6f120dbb3e0a6098d002")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1847db4a0fe70c4bd69df2316f5ab73bcda56ba1")]
 [assembly: System.Reflection.AssemblyProductAttribute("IMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
